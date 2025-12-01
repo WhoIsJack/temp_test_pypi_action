811da8117c527bbab7b6d3ad2cb3b3bbda5eb56a
Jonas Hartmann <jonas.m.hartmann@protonmail.com>
2025-12-01 19:08:13 +0000

    Add all content for test.

---
+++ src/temp-test-pypi-action/manager/manager.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Test file
+"""
+
+
+import os
+import numpy as np
+
+
+def just_a_test(test_input):
+    """Test function.
+    """
+    print(test_input)
+    print(np.arange(15))
+    return test_input
